9972c4ced232d506dae59c4d620a5daee3ff03c7
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2024-03-05 22:46:41 +0000

    Active Link

--- Arquivos/React_Router/src/components/Navbar.jsx
+++ Arquivos/React_Router/src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 // 2 - links com react router
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
-import "./Navbar.module.css";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav>
-        <Link to="/">Home</Link>
+        {/*<Link to="/">Home</Link>
         <Link to="/about">Sobre</Link>
-        <Link to="contact">Contato</Link>
+        <Link to="contact">Contato</Link>*/}
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">Sobre</NavLink>
+        <NavLink to="/contact">Contato</NavLink>
     </nav>
   )
 }
