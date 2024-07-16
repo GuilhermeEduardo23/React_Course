273e435fe937983185459c037e28c0e82c8c4785
Guilherme Eduardo <guilhermeeduardo-17@outlook.com>
2024-07-16 23:26:15 +0100

    Adding styles in components with index.css file

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 
 const pizzaData = [
     {
@@ -48,21 +49,57 @@ const pizzaData = [
 
 function App() {
     return (
-        <div>
-            <h1>Hello React!</h1>
-            <Pizza />
+        <div className='container'>
+            <Header />
+            <Menu />
+            <Footer />
         </div>
     );
 }
 
-function Pizza() {
+function Header() {
+    //const style = {color: 'red', fontSize: '48px', textTransform: 'uppercase'};
+
     return (
-        <div>
-            <img src="pizzas/spinaci.jpg" alt='Pizza spinaci'/>
-            <h2>Pizza Spinaci</h2>
-            <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+        <header className='header'>
+            <h1>Fast React Pizza Co.</h1>
+        </header>
+    );
+}
+
+function Menu() {
+    return (
+        <main className='menu'>
+            <h2>Our menu</h2> 
+            <Pizza
+                name="Pizza Spinaci"
+                ingredient="Tomato, mozarella, spinach, and ricotta cheese"
+                photoName="pizzas/spinaci.jpg"
+                price={10}
+            />
+        </main>
+    ) 
+}
+
+function Pizza(props) {
+    return (
+        <div className='pizza'>
+            <img src={props.photoName} alt={props.name}/>
+            <h3>{props.name}</h3>
+            <p>{props.ingredient}</p>
+            <span>{props.price + 3}</span>
         </div>
     );
+}
+
+function Footer() {
+    return (
+        <footer className='footer'>
+            We're currently open.
+        </footer>
+    );
+    
+    //return React.createElement('footer', null, "We're currently open!");
 }
 
 //React v18
