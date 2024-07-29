a767e2d2b5c22a683c17ce7b2ba5ce7d3b4d5b76
Guilherme Eduardo <guilhermeeduardo-17@outlook.com>
2024-07-29 23:59:34 +0100

    Conditional rendering with ternaries

--- 03-pizza-menu/src/index.js
+++ 03-pizza-menu/src/index.js
@@ -68,15 +68,26 @@ function Header() {
 }
 
 function Menu() {
+    const pizzas = [];
+    const numPizzas = pizzas.length;
+
     return (
         <main className='menu'>
             <h2>Our menu</h2> 
-            <Pizza
+
+            {numPizzas > 0 ? (
+                <ul className='pizzas'>
+                    {pizzas.map((pizza) => (
+                        <Pizza pizzaObj={pizza} key={pizza.name} />
+                    ))}
+                </ul>
+            ) : <p>We're still working on our menu. Please come back later :)</p>}
+            {/* <Pizza
                 name="Pizza Spinaci"
                 ingredient="Tomato, mozarella, spinach, and ricotta cheese"
                 photoName="pizzas/spinaci.jpg"
                 price={10}
-            />
+            /> */}
         </main>
     ) 
 }
@@ -93,9 +104,19 @@ function Pizza(props) {
 }
 
 function Footer() {
+    const hour = new Date().getHours();
+    const openHour = 12;
+    const closeHour = 22;
+    const isOpen = hour >= openHour && hour <= closeHour;
+
     return (
         <footer className='footer'>
-            We're currently open.
+            {isOpen ? (
+                <div className='order'>
+                    <p>We're open until {closeHour}:00. Come visit us or order online.</p>
+                    <button className='btn'>Order</button>
+                </div>
+            ) : <p>We're happy to welcome you between {openHour}:00 and {closeHour}:00.</p>}
         </footer>
     );
     
