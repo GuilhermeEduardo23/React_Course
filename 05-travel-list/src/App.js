57a8fd87b96e1e988c661a15953fe788c26c6874
Guilherme Eduardo <guilhermeeduardo-17@outlook.com>
2024-11-26 23:33:03 +0000

    Challenge Clear List

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -106,6 +106,7 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
         <option value="description">Sort by description</option>
         <option value="packed">Sort by packed status</option>
       </select>
+      <button>Clear list</button>
     </div>
   </div> 
 }
