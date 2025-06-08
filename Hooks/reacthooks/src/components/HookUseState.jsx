bf28ca78f7a5878b7a88392f0f551911b516b33f
Guilherme Eduardo <guilhermeeduardo-17@outlook.com>
2025-06-08 17:53:22 +0100

    Class useState e inputs with form

--- Hooks/reacthooks/src/components/HookUseState.jsx
+++ Hooks/reacthooks/src/components/HookUseState.jsx
@@ -4,19 +4,26 @@ const HookUseState = () => {
   // 1 - useState
   let userName = "João";
   const [name, setName] = useState("João");
-  
+  const [nameRemoved, setNameRemoved] = useState(false);
+
   const changeNames = () => {
     setName("Guilherme");
     setNameRemoved(false);
-
   };
-  
-  const [nameRemoved, setNameRemoved] = useState(false);
 
   const removeName = () => {
     setName("");
     setNameRemoved(true);
   };
+
+  // 2 - useState e input
+  const [age, setAge] = useState(18);
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(age);
+  }
 
   return (
     <div>
@@ -46,6 +53,18 @@ const HookUseState = () => {
             Clique aqui para remover o nome alterado!
           </button>
         )}
+
+        {/* 2 - useState e input */}
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={age}
+            onChange={(e) => setAge(e.target.value)}
+            placeholder="Digite sua idade"
+          />
+          <button type="submit">Enviar</button>
+        </form>
+        <p>Você tem {age} anos!</p>
       </div>
     </div>
   );
