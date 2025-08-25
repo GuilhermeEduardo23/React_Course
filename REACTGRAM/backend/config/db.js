eb380922a230cf1f8521d9077f64e13529003153
Guilherme Eduardo - Dr. Bernard <guilherme.silva@drbernard.org>
2025-08-25 16:46:26 +0100

    Criando Model do usuário

--- REACTGRAM/backend/config/db.js
+++ REACTGRAM/backend/config/db.js
@@ -3,15 +3,17 @@ const dbUser = process.env.DB_USER;
 const dbPassword = process.env.DB_PASS;
 
 const conn = async () => {
-    try {
-        const dbConn = await mongoose.connect(`mongodb+srv://${dbUser}:${dbPassword}@cluster0.qmzftm4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`);
+  try {
+    const dbConn = await mongoose.connect(
+      `mongodb+srv://${dbUser}:${dbPassword}@cluster0.qmzftm4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
 
-        console.log("Conectou ao banco!");
+    console.log("Conectou ao banco!");
 
-        return dbConn;
-    } catch (error) {
-        console.log(error);
-    }
+    return dbConn;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 conn();
