318c08a3f15845b3d26756115156c815cbc92e32
Guilherme Eduardo <guilhermeeduardo-17@outlook.com>
2025-10-18 23:05:57 +0100

    CSS Navbar e Footer

--- REACTGRAM/frontend/src/components/Navbar.jsx
+++ REACTGRAM/frontend/src/components/Navbar.jsx
@@ -10,17 +10,25 @@ import {
 } from "react-icons/bs";
 
 const Navbar = () => {
-  return <nav>
-    <Link to="/">ReactGram</Link>
-    <form>
+  return (
+    <nav id="nav">
+      <Link to="/">ReactGram</Link>
+      <form id="search-form">
         <BsSearch />
-        <input type="text" />
-        <ul id="nav-links">
-            <NavLink to="/"> <BsHouseDoorFill /> Entrar </NavLink>
-            <NavLink to="/register">Cadastrar</NavLink>
-        </ul>
-    </form>
-  </nav>;
+        <input type="text" placeholder="Pesquisar" />
+      </form>
+      <ul id="nav-links">
+        <li>
+          <NavLink to="/">
+            <BsHouseDoorFill /> Entrar
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/register">Cadastrar</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Navbar;
