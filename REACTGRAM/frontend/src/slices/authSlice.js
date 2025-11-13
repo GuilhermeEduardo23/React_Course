911f79245a62af6c236c23575c39759a2c58b444
Guilherme Eduardo <guilhermeeduardo-17@outlook.com>
2025-11-13 17:56:50 +0000

    Implementando lógica de logout do usuário

--- REACTGRAM/frontend/src/slices/authSlice.js
+++ REACTGRAM/frontend/src/slices/authSlice.js
@@ -22,6 +22,11 @@ export const register = createAsyncThunk(
   }
 );
 
+// Logout an user
+export const logout = createAsyncThunk("auth/logout", async () => {
+  await authService.logout();
+});
+
 export const authSlice = createSlice({
   name: "auth",
   initialState,
@@ -44,6 +49,12 @@ export const authSlice = createSlice({
       .addCase(register.rejected, (state, action) => {
         (state.loading = false),
           (state.error = action.payload),
+          (state.user = null);
+      })
+      .addCase(logout.fulfilled, (state) => {
+        (state.loading = false),
+          (state.success = true),
+          (state.error = null),
           (state.user = null);
       });
   },
