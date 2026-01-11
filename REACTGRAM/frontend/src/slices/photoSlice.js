f421ce230c7c36786f13c201d18fa10852b85e04
Guilherme Eduardo <guilhermeeduardo-17@outlook.com>
2026-01-11 17:02:14 +0000

    photo insertion slice

--- REACTGRAM/frontend/src/slices/photoSlice.js
+++ REACTGRAM/frontend/src/slices/photoSlice.js
@@ -11,7 +11,18 @@ const initialState = {
   message: null,
 };
 
-// functions
+// Publish user photo
+export const publishPhoto = createAsyncThunk(
+  "photo/publish",
+  async (photo, thunkAPI) => {
+    const token = thunkAPI.getState().auth.user.token;
+    const data = await photoService.publishPhoto(photo, token);
+
+    if (data) return data;
+
+    return thunkAPI.rejectWithValue(data.errors[0]);
+  }
+);
 
 export const photoSlice = createSlice({
   name: "photo",
@@ -20,6 +31,25 @@ export const photoSlice = createSlice({
     resetMessage: (state) => {
       state.message = null;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(publishPhoto.pending, (state) => {
+        (state.loading = true), (state.error = false);
+      })
+      .addCase(publishPhoto.fulfilled, (state, action) => {
+        (state.loading = false),
+          (state.success = true),
+          (state.error = null),
+          (state.photo = action.payload),
+          state.photos.unshift(state.photo),
+          (state.message = "Foto publicada com sucesso!");
+      })
+      .addCase(publishPhoto.rejected, (state, action) => {
+        (state.loading = false),
+          (state.error = action.payload),
+          (state.photo = {});
+      });
   },
 });
 
